a9b974e2fdd99c57f00799ed7e17cd29f7594cc8
dmitriiii <dimitriy1988wl@gmail.com>
2021-01-09 20:15:13 +0300

    updates

--- src/App.js
+++ src/App.js
@@ -19,7 +19,6 @@ class App extends React.Component {
       smtp_subject: '',
       smtp_body: ''
     };
-    this.handleChangeArea = this.handleChangeArea.bind(this);
     this.handleClickParse = this.handleClickParse.bind(this);
     this.handleClickClear = this.handleClickClear.bind(this);
     this.handleClickRemove = this.handleClickRemove.bind(this);
@@ -27,31 +26,17 @@ class App extends React.Component {
     this.fetchSingleEmail = this.fetchSingleEmail.bind(this);
     
     
-    
-    
     this.handleClickSmtpDefault = this.handleClickSmtpDefault.bind(this);
-    this.handleChangeSmtpFrom = this.handleChangeSmtpFrom.bind(this);
-    this.handleChangeSmtpEmail = this.handleChangeSmtpEmail.bind(this);
-    this.handleChangeSmtpPass = this.handleChangeSmtpPass.bind(this);
-    this.handleChangeSmtpReply = this.handleChangeSmtpReply.bind(this);
-    this.handleChangeSmtpSubject = this.handleChangeSmtpSubject.bind(this);
-    this.handleChangeSmtpBody = this.handleChangeSmtpBody.bind(this);
+    this.handleChangeFields = this.handleChangeFields.bind(this);
   }
 
   componentDidMount() {
     this.handleClickSmtpDefault();
   }
-  // Changes in textarea
-  handleChangeArea(event){
-    this.setState({
-      areaValue: event.target.value
-    });
-  }
   
   handleClickParse(){
     let extractedEmails = this.extractEmails(this.state.areaValue);
     extractedEmails = this.removeDuplicates(extractedEmails);
-
     this.setState({
       emails: this.getEmailsObj(extractedEmails),
       areaValue: extractedEmails.join(" | ")
@@ -66,50 +51,68 @@ class App extends React.Component {
     });
   }
 
+
   handleClickRemove(index) {
     const email = this.state.emails[index].email;
     const areaValue = this.state.areaValue.replace(email, '');
-    
+
     let extractedEmails = this.extractEmails(areaValue);
     extractedEmails = this.removeDuplicates(extractedEmails);
-    
+
     this.setState({
       emails: this.getEmailsObj(extractedEmails),
       areaValue: extractedEmails.join(" | ")
     });
   }
 
+  // Send Request single
   async fetchSingleEmail(email,index){
+    const url = 'http://dev.pro24web.site/bulk_email/email.php';
+    const smtp = {
+      smtp_body: this.state.smtp_body,
+      smtp_email: this.state.smtp_email,
+      smtp_from: this.state.smtp_from,
+      smtp_pass: this.state.smtp_pass,
+      smtp_reply: this.state.smtp_reply,
+      smtp_subject: this.state.smtp_subject
+    }
     try {
-      const res = await fetch('http://dev.pro24web.site/', {
+      const res = await fetch(url, {
         method: 'POST',
         headers: {
           'Content-Type' : 'application/json'
         },
-        // mode: 'no-cors',
-        body: JSON.stringify({ email, index })
+        body: JSON.stringify({ email, index, smtp })
       });
       const data = await res.json();
       
+      //Successfully sent
+      // if( data.type==='success' ){
+        let emails = this.state.emails;
+        // console.log(emails);
+        emails[data.emails_index].status = data.emails_status;
+        emails[data.emails_index].classcss = data.emails_classcss;
+        
+        this.setState({
+          emails: emails
+        });
+      // }
+
     } catch (error) {
       console.error(error);
     }
   }
 
+  //Sending massive emails
   async handleClickBulk(){
     const emails = this.state.emails;
     for (let index = 0; index < emails.length; index++) {
       const emailData = emails[index];
       await this.fetchSingleEmail(emailData.email, index);
     }
-
-    // emails.forEach( async (emailData, index) => {
-    //   await this.fetchSingleEmail(emailData.email, index);
-    // });
   }
 
-
-  // SMTP Events
+  // Set default SMTP data
   handleClickSmtpDefault(){
     this.setState({
       smtp_from: 'Martin',
@@ -119,42 +122,22 @@ class App extends React.Component {
       smtp_subject: 'Your NordVPN account has been hacked',
     });
   }
-  handleChangeSmtpFrom(event){
-    this.setState({
-      smtp_from: event.target.value
-    });
-  }
-  handleChangeSmtpEmail(event){
-    this.setState({
-      smtp_email: event.target.value
-    });
-  }
-  handleChangeSmtpPass(event){
-    this.setState({
-      smtp_pass: event.target.value
-    });
-  }
-  handleChangeSmtpReply(event){
-    this.setState({
-      smtp_reply: event.target.value
-    });
-  }
-  handleChangeSmtpSubject(event){
-    this.setState({
-      smtp_subject: event.target.value
-    });
-  }
-  handleChangeSmtpBody(event){
-    this.setState({
-      smtp_body: event.target.value
-    });
-  }
 
+  // Universal changing input/textarea and update states
+  handleChangeFields(event){
+    const name = event.target.name;
+    const value = event.target.value;
+    const obj = {};
+    obj[name] = value;    
+    this.setState(obj);
+  }
+ 
   // Retrieve emails from inserted text
   extractEmails(text){
     return text.match(/([a-zA-Z0-9._-]+@[a-zA-Z0-9._-]+\.[a-zA-Z0-9._-]+)/gi);
   }
-  //Remove duplicate emails
+
+  //Remove duplicate emails from inserted text
   removeDuplicates(arr) {
     var obj = {};
     var ret_arr = [];
@@ -166,13 +149,15 @@ class App extends React.Component {
     }
     return ret_arr;
   }
+
   //Prepare emails object for state
   getEmailsObj(emails, status='Not Started'){
     let emailsObj = [];
     emails.forEach(function (item, index) {
       emailsObj.push({
         email: item,
-        status: status
+        status: status,
+        classcss: ''
       })
     });
     return emailsObj;
@@ -182,16 +167,17 @@ class App extends React.Component {
   
   render() {
     const areaValue = this.state.areaValue;
-    const rows = this.state.emails.map((email,index) =>
-    <tr key={index}>
-      <th scope="row">{index+1}</th>
-      <td>{email.email}</td>
-      <td>{email.status}</td>
-      <td>
-        <button type="button" className="btn btn-sm btn-secondary" onClick={() => this.fetchSingleEmail(email.email, index)}><i className="fas fa-redo"></i> Resend</button>
-        <button type="button" className="btn btn-sm btn-danger" onClick={() => this.handleClickRemove(index)}><i className="far fa-trash-alt"></i></button>
-      </td>
-    </tr>
+    const emails = this.state.emails;
+    const rows = emails.map((email,index) =>
+      <tr key={index} className={ email.classcss }>
+        <th scope="row">{index+1}</th>
+        <td>{email.email}</td>
+        <td>{email.status}</td>
+        <td>
+          <button type="button" className="btn btn-sm btn-secondary" onClick={() => this.fetchSingleEmail(email.email, index)}><i className="fas fa-redo"></i> Resend</button>
+          <button type="button" className="btn btn-sm btn-danger" onClick={() => this.handleClickRemove(index)}><i className="far fa-trash-alt"></i></button>
+        </td>
+      </tr>
     );
     return (
       <div className="App">
@@ -199,27 +185,27 @@ class App extends React.Component {
           <h4>SMTP Settings</h4>
           <div className="form-group">
             <small className="form-text text-muted">Name ( from )</small>
-            <input type="text" className="form-control form-control-sm" value={this.state.smtp_from} onChange={this.handleChangeSmtpFrom} />
+            <input type="text" name="smtp_from" className="form-control form-control-sm" value={this.state.smtp_from} onChange={this.handleChangeFields} />
           </div>
 
           <div className="form-group">
             <small className="form-text text-muted">Yandex smtp email</small>
-            <input type="text" className="form-control form-control-sm" value={this.state.smtp_email} onChange={this.handleChangeSmtpEmail} />
+            <input type="text" name="smtp_email" className="form-control form-control-sm" value={this.state.smtp_email} onChange={this.handleChangeFields} />
           </div>
 
           <div className="form-group">
             <small className="form-text text-muted">Yandex smtp pass</small>
-            <input type="text" className="form-control form-control-sm" value={this.state.smtp_pass} onChange={this.handleChangeSmtpPass} />
+            <input type="text" name="smtp_pass" className="form-control form-control-sm" value={this.state.smtp_pass} onChange={this.handleChangeFields} />
           </div>
 
           <div className="form-group">
             <small className="form-text text-muted">Reply to</small>
-            <input type="text" className="form-control form-control-sm" value={this.state.smtp_reply} onChange={this.handleChangeSmtpReply} />
+            <input type="text" name="smtp_reply" className="form-control form-control-sm" value={this.state.smtp_reply} onChange={this.handleChangeFields} />
           </div>
 
           <div className="form-group">
             <small className="form-text text-muted">Subject</small>
-            <input type="text" className="form-control form-control-sm" value={this.state.smtp_subject} onChange={this.handleChangeSmtpSubject}/>
+            <input type="text" name="smtp_subject" className="form-control form-control-sm" value={this.state.smtp_subject} onChange={this.handleChangeFields}/>
           </div>
 
           <div className="form-group">
@@ -227,8 +213,9 @@ class App extends React.Component {
             <textarea 
               className="form-control" 
               rows="5" 
+              name="smtp_body"
               value={this.state.smtp_body} 
-              onChange={this.handleChangeSmtpBody}
+              onChange={this.handleChangeFields}
             >
             </textarea>
           </div>
@@ -237,26 +224,30 @@ class App extends React.Component {
         <div className="container">
           <div className="row">
             <div className="col-md-12">
-              <h2>Bulk Sending emails</h2>
+              <h3>Bulk Sending emails</h3>
+              
               <div className="form-group">
-                
+                <textarea 
+                  className="form-control mb-2" 
+                  rows="5"
+                  value={areaValue} 
+                  name='areaValue'
+                  onChange={this.handleChangeFields}
+                  placeholder="Paste emails here">
+                </textarea>
                 { areaValue.length === 0  
-                  ?<button type="button" className="btn btn-info btn-sm disabled" disabled><i className="fas fa-paste"></i> Paste emails</button>
+                  ?<React.Fragment></React.Fragment>
                   :<React.Fragment>
                     <button type="button" className="btn btn-info btn-sm" onClick={this.handleClickParse}><i className="fas fa-hat-wizard"></i> Parse</button>
                     <button type="button" className="btn btn-sm btn-light" onClick={this.handleClickClear}><i className="fas fa-broom"></i> Clear All</button>
                   </React.Fragment>
                 }
-                
-                <button type="button" className="btn btn-primary btn-sm" onClick={this.handleClickBulk}><i className="fas fa-mail-bulk"></i> Bulk Send</button>
-                <hr/>
-                <textarea 
-                  className="form-control" 
-                  rows="5" 
-                  value={this.state.areaValue} 
-                  onChange={this.handleChangeArea}
-                  placeholder="Paste emails here">
-                </textarea>
+                { emails.length === 0  
+                  ?<React.Fragment></React.Fragment>
+                  :<React.Fragment>
+                    <button type="button" className="btn btn-primary btn-sm" onClick={this.handleClickBulk}><i className="fas fa-mail-bulk"></i> Bulk Send</button>
+                  </React.Fragment>
+                }
               </div>
               <h5 className={styles.results}>
                 <span>Results</span>  
